b71c233f1b7645cf7198151a1369ff9cd752b699
Filip Jelic <jelic.filip@gmail.com>
2019-06-02 23:08:11 +0200

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# React D3 Examples
-
-D3 & React playground [Live Demo](https://flipio.github.io/react-d3-examples/)
-
-
---
+++ precache-manifest.590e763c9ba2327ebec570c59303ea5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20d19b3ae919b61743ae",
+    "url": "/react-d3-examples/static/js/runtime~main.20d19b3a.js"
+  },
+  {
+    "revision": "57ea5c2753c8a4a24c99",
+    "url": "/react-d3-examples/static/js/main.57ea5c27.chunk.js"
+  },
+  {
+    "revision": "98240347fde5aa41794b",
+    "url": "/react-d3-examples/static/js/1.98240347.chunk.js"
+  },
+  {
+    "revision": "57ea5c2753c8a4a24c99",
+    "url": "/react-d3-examples/static/css/main.2baaaa7e.chunk.css"
+  },
+  {
+    "revision": "fb91f17f53e474bfb1db0ae5c3337a82",
+    "url": "/react-d3-examples/index.html"
+  }
+];
